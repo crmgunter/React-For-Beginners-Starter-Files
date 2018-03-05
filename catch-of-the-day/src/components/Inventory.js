0c72114fae08c0b464038a9a2cff2fdb89ea5497
crmgunter <crmgunter@gmail.com>
2018-03-04 23:59:42 -0500

    forgot to commit for a while. whatever, it's a code along

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import AddFishForm from './AddFishForm'
 
 class Inventory extends Component {
     render() {
         return (
             <div className="inventory">
-            <h1>Inventory!</h1>
+            <h2>Inventory</h2>
+                <AddFishForm addFish={this.props.addFish}/>
                 
+                <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
             </div>
         );
     }
---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+const NotFound = () => (
+    <div>
+        <h2>Not Found!!!!</h2>
+    </div>
+)
+
+// class NotFound extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h2>Not Found!!!</h2>
+//             </div>
+//         );
+//     }
+// }
+
+export default NotFound;
